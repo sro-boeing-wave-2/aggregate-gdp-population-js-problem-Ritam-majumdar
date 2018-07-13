6f3f82acdba82b8af0de3d90dbcad96297d0a378
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-07-13 17:26:31 +0530

    Modified aggrgate.js, used set for continents

--- aggregate.js
+++ aggregate.js
@@ -10,6 +10,12 @@ const aggregate = () => {
   let i;
   let contIndex;
   const continent = ['South America', 'Oceania', 'North America', 'Asia', 'Europe', 'Africa'];
+  const africa = new Set(['South Africa']);
+  const asia = new Set(['China', 'India', 'Indonesia', 'Japan', 'Russia', 'Republic of Korea', 'Turkey', 'Saudi Arabia']);
+  const oceania = new Set(['Australia']);
+  const europe = new Set(['France', 'Germany', 'Italy', 'United Kingdom']);
+  const northAmerica = new Set(['Canada', 'Mexico', 'USA']);
+  const southAmerica = new Set(['Brazil', 'Argentina']);
   for (i = 0; i <= 5; i += 1) {
     continentData[continent[i]] = {
       GDP_2012: 0, POPULATION_2012: 0,
@@ -20,27 +26,27 @@ const aggregate = () => {
     const csvRows = csvTextProcessed.split('\n');
     for (i = 1; i < csvRows.length - 1; i += 1) {
       const countryData = csvRows[i].split(',');
-      if (countryData[0] === 'Canada' || countryData[0] === 'Mexico' || countryData[0] === 'USA') {
+      if (northAmerica.has(countryData[0])) {
         contIndex = 2;
         continentData[continent[contIndex]].GDP_2012 += parseFloat(countryData[7]);
         continentData[continent[contIndex]].POPULATION_2012 += parseFloat(countryData[4]);
-      } else if (countryData[0] === 'Brazil' || countryData[0] === 'Argentina') {
+      } else if (southAmerica.has(countryData[0])) {
         contIndex = 0;
         continentData[continent[contIndex]].GDP_2012 += parseFloat(countryData[7]);
         continentData[continent[contIndex]].POPULATION_2012 += parseFloat(countryData[4]);
-      } else if (countryData[0] === 'China' || countryData[0] === 'India' || countryData[0] === 'Indonesia' || countryData[0] === 'Japan' || countryData[0] === 'Saudi Arabia' || countryData[0] === 'Russia' || countryData[0] === 'Republic of Korea' || countryData[0] === 'Turkey') {
+      } else if (asia.has(countryData[0])) {
         contIndex = 3;
         continentData[continent[contIndex]].GDP_2012 += parseFloat(countryData[7]);
         continentData[continent[contIndex]].POPULATION_2012 += parseFloat(countryData[4]);
-      } else if (countryData[0] === 'South Africa') {
+      } else if (africa.has(countryData[0])) {
         contIndex = 5;
         continentData[continent[contIndex]].GDP_2012 += parseFloat(countryData[7]);
         continentData[continent[contIndex]].POPULATION_2012 += parseFloat(countryData[4]);
-      } else if (countryData[0] === 'France' || countryData[0] === 'Germany' || countryData[0] === 'Italy' || countryData[0] === 'United Kingdom') {
+      } else if (europe.has(countryData[0])) {
         contIndex = 4;
         continentData[continent[contIndex]].GDP_2012 += parseFloat(countryData[7]);
         continentData[continent[contIndex]].POPULATION_2012 += parseFloat(countryData[4]);
-      } else if (countryData[0] === 'Australia') {
+      } else if (oceania.has(countryData[0])) {
         contIndex = 1;
         continentData[continent[contIndex]].GDP_2012 += parseFloat(countryData[7]);
         continentData[continent[contIndex]].POPULATION_2012 += parseFloat(countryData[4]);
