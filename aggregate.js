95855c424f03a47170eb3ae59e7082bcae29bc49
Ritam Majjumdar <majumdar_ritam@yahoo.com>
2018-07-17 10:17:30 +0530

    Added continentCountries.json and used it as mapper, optimized the code, only one for loop is used

--- aggregate.js
+++ aggregate.js
@@ -3,53 +3,30 @@
  * @param {*} filePath
  */
 const fs = require('fs');
+const mapper = require('./continentCountries.json');
 
 const aggregate = () => new Promise((resolve) => {
   const myReadStream = fs.createReadStream('/aggregate-gdp-population-js-problem-Ritam-majumdar/data/datafile.csv', 'utf8');
   const continentData = {};
   let i;
-  let contIndex;
-  const continent = ['South America', 'Oceania', 'North America', 'Asia', 'Europe', 'Africa'];
-  const africa = new Set(['South Africa']);
-  const asia = new Set(['China', 'India', 'Indonesia', 'Japan', 'Russia', 'Republic of Korea', 'Turkey', 'Saudi Arabia']);
-  const oceania = new Set(['Australia']);
-  const europe = new Set(['France', 'Germany', 'Italy', 'United Kingdom']);
-  const northAmerica = new Set(['Canada', 'Mexico', 'USA']);
-  const southAmerica = new Set(['Brazil', 'Argentina']);
-  for (i = 0; i <= 5; i += 1) {
-    continentData[continent[i]] = {
-      GDP_2012: 0, POPULATION_2012: 0,
-    };
-  }
   myReadStream.on('data', (chunk) => {
     const csvTextProcessed = chunk.replace(/['"]+/g, '');
     const csvRows = csvTextProcessed.split('\n');
-    for (i = 1; i < csvRows.length - 1; i += 1) {
+    const csvHeaders = csvRows[0].split(',');
+    const countryNameIndex = csvHeaders.indexOf('Country Name');
+    const populationIndex = csvHeaders.indexOf('Population (Millions) - 2012');
+    const gdpIndex = csvHeaders.indexOf('GDP Billions (US Dollar) - 2012');
+    for (i = 1; i < csvRows.length - 2; i += 1) {
       const countryData = csvRows[i].split(',');
-      if (northAmerica.has(countryData[0])) {
-        contIndex = 2;
-        continentData[continent[contIndex]].GDP_2012 += parseFloat(countryData[7]);
-        continentData[continent[contIndex]].POPULATION_2012 += parseFloat(countryData[4]);
-      } else if (southAmerica.has(countryData[0])) {
-        contIndex = 0;
-        continentData[continent[contIndex]].GDP_2012 += parseFloat(countryData[7]);
-        continentData[continent[contIndex]].POPULATION_2012 += parseFloat(countryData[4]);
-      } else if (asia.has(countryData[0])) {
-        contIndex = 3;
-        continentData[continent[contIndex]].GDP_2012 += parseFloat(countryData[7]);
-        continentData[continent[contIndex]].POPULATION_2012 += parseFloat(countryData[4]);
-      } else if (africa.has(countryData[0])) {
-        contIndex = 5;
-        continentData[continent[contIndex]].GDP_2012 += parseFloat(countryData[7]);
-        continentData[continent[contIndex]].POPULATION_2012 += parseFloat(countryData[4]);
-      } else if (europe.has(countryData[0])) {
-        contIndex = 4;
-        continentData[continent[contIndex]].GDP_2012 += parseFloat(countryData[7]);
-        continentData[continent[contIndex]].POPULATION_2012 += parseFloat(countryData[4]);
-      } else if (oceania.has(countryData[0])) {
-        contIndex = 1;
-        continentData[continent[contIndex]].GDP_2012 += parseFloat(countryData[7]);
-        continentData[continent[contIndex]].POPULATION_2012 += parseFloat(countryData[4]);
+      const countryGdp = parseFloat(countryData[gdpIndex]);
+      const countryPopulation = parseFloat(countryData[populationIndex]);
+      if (continentData[mapper[countryData[countryNameIndex]]] === undefined) {
+        continentData[mapper[countryData[countryNameIndex]]] = {};
+        continentData[mapper[countryData[countryNameIndex]]].GDP_2012 = countryGdp;
+        continentData[mapper[countryData[countryNameIndex]]].POPULATION_2012 = countryPopulation;
+      } else {
+        continentData[mapper[countryData[countryNameIndex]]].GDP_2012 += countryGdp;
+        continentData[mapper[countryData[countryNameIndex]]].POPULATION_2012 += countryPopulation;
       }
     }
     fs.writeFile('./output/output.json', JSON.stringify(continentData), () => {
